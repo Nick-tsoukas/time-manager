3b9ada6f7789738719ebc746974a2120ae6057a5
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2017-04-06 12:41:04 -0500

    update

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+(function () {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+        
+        $stateProvider
+            .state('clock', {
+            url: '/',
+            templateUrl: '/templates/clock.html',
+            controller: 'ClockCtrl as clock'
+        });
+       
+    }
+
+
+    angular
+        .module('timeApp', ['ui.router', 'firebase'])
+        .config(config);
+})();
